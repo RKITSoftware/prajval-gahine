c37fa3ac21706c8428274e685733a650b59551d2
Prajval Gahine <Prajval.G@dev.rkitsoft.local>
2024-05-01 18:28:56 +0530

    Started with devextreme

---
+++ 08 DevExtreme/DevExtreme/WebApplication1/index.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    var container = $("#container");
+
+    // create a div inside container with classname "dx-button-content"
+    container.dxButton();
+
+    // create an instance of button inside "dx-button-content"
+    var button = container.dxButton("instance");
+
+    // sets value of the option specified
+    button.option("text", "Submit");
+
+    //button.option("onClick", function () {
+    //    alert("Hello world");
+    //})
+
+    $("#container").dxButton().focus(function () {
+        console.log("Focus using dxButton().");
+    });
+
+    button.focus(function () {
+        console.log("Focus using button.");
+    });
+
+    //button.focus();
+});
